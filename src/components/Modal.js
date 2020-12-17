233f4b5efb41b32c965585994ca9ccace5b02559
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-18 00:28:19 +0530

    Added Floating action button

---
+++ src/components/Modal.js
@@ -0,0 +1,87 @@
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Dialog from '@material-ui/core/Dialog';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import Draggable from 'react-draggable';
+
+const useStyles = makeStyles((theme) => ({
+    fab: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        width: 500,
+        position: 'relative',
+        minHeight: 200,
+      },
+  }));
+  
+
+function PaperComponent(props) {
+  return (
+    <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
+export default function Modal() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Fab color="primary" 
+      aria-label="add" 
+      size = "medium" 
+      onClick={handleClickOpen}
+      className ={classes.fab}>
+        <AddIcon />
+        </Fab> 
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          Subscribe
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import Modal from './Modal';
 
 const Notes = () => {
     
     return (
         <Container maxWidth = 'sm'>
             No Notes Available.
+            <Modal/>
         </Container >
     )
 }
