c49ea6c55985b6ec8e4c513ba67a872353b488f5
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-23 02:20:18 +0530

    Added styles

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,9 +1,26 @@
 import React , {useState , useEffect} from 'react';
 import { Container } from '@material-ui/core';
 import Modal from './Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      margin: 15,
+      flexGrow: 1,
+    },
+    title: {
+      fontSize: 14,
+    },
+  });
 
 const Notes = () => {
-    
+    const classes = useStyles();
     const [notes , setNotes] = useState([]);
 
     const addNote = (note) => {
@@ -20,13 +37,24 @@ const Notes = () => {
       }, [])
 
     return (
+
         <Container maxWidth = 'sm' >
             {
             notes !== null ? notes.map((note,index) => (
-            <div key = {index}> 
-            <h1>{note.title}</h1>
-            <h2>{note.desc}</h2>
-            </div>)
+            <Grid container spacing={2}>
+            <Grid item xs>
+            <Card className={classes.root} key={index} > 
+            <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                {note.title}
+            </Typography>
+            <Typography variant="body2" component="p">
+                {note.desc}
+            </Typography>
+            </CardContent>
+            </Card>
+            </Grid>
+            </Grid>)
             ) :
              console.log('hello')}
             <Modal addNote = {addNote}/>
